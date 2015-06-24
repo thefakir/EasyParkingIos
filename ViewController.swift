a9ab24ef80a78c839856f01ff03d035a5620d1fb
internet <internet@internets-Mac-2.local>
2015-06-23 19:52:11 -0700

    add all files

---
+++ ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  webApi3
+//
+//  Created by internet on 5/20/15.
+//  Copyright (c) 2015 internet. All rights reserved.
+//
+
+import UIKit
+import SwiftHTTP
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       /* var parser = Parser();
+        for item in parser.getParkings()
+        {
+        println(item.ParkingName)
+        // Do any additional setup after loa    "http://www.easyparking.somee.com/api/parking"
+    }
+        */
+        
+        
+        
+        
+        
+        }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+      
+    }
+
+
+}
+
